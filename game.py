ee1146c859f7cfc723c5b250754c30ffce0d1a17
biancaballer <m261212@usna.edu>
2023-10-13 09:39:01 -0400

    basic game working

--- game.py
+++ game.py
@@ -1,16 +1,20 @@
 import pygame
 import time
+import sys
 
 pygame.init()
 
 screen = pygame.display.set_mode((400,400))
 pygame.display.set_caption("Chomp!")
-screen.fill((106,238,252))
+screen.fill((106, 238, 252))
 
-pygame.draw.rect(screen, (100,25,0), (0,380,400,400))
+pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
 pygame.display.flip()
 while True:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
-            print("not today! this game runs 4ever")
+            print("thanks for playing!")
+            pygame.quit()
+            sys.exit()
+
