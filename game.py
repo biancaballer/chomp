a3ec2e4b09949dfbf55bd72b59b9f0d1f8429485
biancaballer <m261212@usna.edu>
2023-10-18 09:27:32 -0400

    use parametric variables

--- game.py
+++ game.py
@@ -2,15 +2,26 @@ import pygame
 import time
 import sys
 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+SAND_HEIGHT = 20
+TILE_SIZE = 64  # tiles are square height == width
+
+# COLORS
+WATER_COLOR = (106, 238, 252)
+SAND_COLOR = (100, 25, 0)
 pygame.init()
 
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
-screen.fill((106, 238, 252))
+screen.fill(WATER_COLOR)
 
-pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
+pygame.draw.rect(screen, SAND_COLOR,
+                 (0, SCREEN_HEIGHT-SAND_HEIGHT, SCREEN_WIDTH, SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand, (200, 200, 64, 64))
+screen.blit(sand,
+            (SCREEN_WIDTH / 2 - TILE_SIZE / 2,
+             SCREEN_HEIGHT / 2 - TILE_SIZE / 2))
 while True:
     for event in pygame.event.get():
 
@@ -19,4 +30,3 @@ while True:
             pygame.quit()
             sys.exit()
         pygame.display.flip()
-
