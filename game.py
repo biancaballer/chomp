b6ce4107413e9eb368772c65453c9da9575b0bbe
biancaballer <m261212@usna.edu>
2023-10-19 08:52:40 -0400

    added game fonts

--- game.py
+++ game.py
@@ -1,17 +1,12 @@
 import pygame
-import time
 import sys
+import random
 
-# DIMENSIONS
-TILE_SIZE = 64  # tiles are square height == width
-SCREEN_WIDTH = 8 * TILE_SIZE
-SCREEN_HEIGHT = 8 * TILE_SIZE
-SAND_HEIGHT = 20
+from settings import *  # imports variables from settings keeps code clean
 
-# COLORS
-WATER_COLOR = (106, 238, 252)
-SAND_COLOR = (100, 25, 0)
 pygame.init()
+
+game_font = pygame.font.Font("assets/fonts/Black_Crayon.ttf", 128)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Chomp!")
@@ -22,10 +17,27 @@ pygame.draw.rect(screen, SAND_COLOR,
 sand = pygame.image.load("assets/images/sand.png").convert()
 sand_top = pygame.image.load("assets/images/sand_top.png").convert()
 sand_top.set_colorkey((0, 0, 0))
+seagrass = pygame.image.load("assets/images/seagrass.png").convert()
+seagrass.set_colorkey((0, 0, 0))
+
+# sandy bottom
 for i in range(SCREEN_WIDTH // TILE_SIZE):   # double divide for int
     screen.blit(sand, (TILE_SIZE * i, SCREEN_HEIGHT-TILE_SIZE))
     screen.blit(sand_top, (TILE_SIZE * i, SCREEN_HEIGHT-(2*TILE_SIZE)))
+# randomly place 4 pieces of grass along the bottom of the screen
+for _ in range(4):
+    x = random.randint(0, SCREEN_WIDTH) - (0.5 * TILE_SIZE)
+    # offset the seagrass so it looks better
+    y = (random.randint(SCREEN_HEIGHT - 2*TILE_SIZE, SCREEN_HEIGHT)
+         - (0.5 * TILE_SIZE))
+    screen.blit(seagrass, (x, y))
+# blit  in water tiles
+# draw the CHOMP! title
+text = game_font.render("CHOMP!", True, (255, 69, 0))
+screen.blit(text, (SCREEN_WIDTH // 2 - text.get_width() // 2,
+                   SCREEN_HEIGHT // 2 - text.get_height() // 2))
 
+pygame.display.flip()
 while True:
     for event in pygame.event.get():
 
@@ -33,4 +45,3 @@ while True:
             print("thanks for playing!")
             pygame.quit()
             sys.exit()
-        pygame.display.flip()
